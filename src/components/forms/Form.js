84fad8660b9bbc9793c3f6c3e9131b0c358cc7ae
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-09-05 12:52:48 +0700

    delete ItemCurrency components loop result

--- src/components/forms/Form.js
+++ src/components/forms/Form.js
@@ -1,17 +1,41 @@
 import React from 'react'
-import ItemCurrency from '../forms/ItemCurrency'
+import { Consumer } from '../../context';
 
 const Form = (props) => {
-    const  { rates } = props
     return (
-        <div className='col-md-8'>
-            <div className='card mb-4 shadow-sm'>
-                <div className="card-header">
-                    USD <span className='angka'>1</span>
-                </div>
-                <ItemCurrency rates={rates}/>
-            </div>
-        </div>
+        <Consumer>
+            {value => {
+            const { rates } = value
+            const a = []
+            for (const i in rates) {
+                a.push(
+                    <div className='card-body'>
+                        <span className='angka'>
+                            {rates[i]}
+                            <button type="button" className="btn btn-danger" style={{marginLeft:'10px'}} onClick={this.onClickClose}>
+                                &minus;
+                            </button>
+                        </span>
+                        <h5>
+                            {Object.keys(rates).find(key => rates[key] === rates[i])}
+                            { /* hehehe */ }
+                        </h5>
+                        <br/>
+                    </div>
+                )
+            }
+                return (
+                    <div className='col-md-8'>
+                        <div className='card mb-4 shadow-sm'>
+                            <div className="card-header">
+                                USD <span className='angka'>1</span>
+                            </div>
+                            {a}
+                        </div>
+                    </div>
+                )
+            }}
+        </Consumer>
     )
 }
 
