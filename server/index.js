5834f79379c2249cdc0001b5f50085d703460049
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-09-09 16:18:08 +0700

    comment server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const port = 4000
+/*const port = 4000
 const express = require('express')
 const compression = require('compression')
 const basicAuth = require('express-basic-auth')
@@ -19,3 +19,4 @@ app.get('/*', (req,res) => res.sendFile('./build/index.html'))
 app.listen(port, '0.0.0.0', () => {
     console.log(`Listening on http://0.0.0.0:${port}`)
 })
+*/
